cfdae8f4bc87af74aa382bd38ef4096d2d5c1230
nikitamobile <nikitamobile@gmail.com>
2023-01-24 09:30:56 +0500

    init: Reverse-int task submission

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function reverse (n) {
+
+let input = n;    
+var positive = Math.abs(input);
+let myFunc = num => Number(num);  
+var arr = Array.from(String(positive), myFunc);
+rev = arr.reverse();
+
+
+arrInt = Number(rev.join(''));
+
+return(arrInt);
   
 }
+
+
+/*
+
+let input = 170;
+
+var positive = Math.abs(input);
+let myFunc = num => Number(num);  
+var arr = Array.from(String(positive), myFunc);
+reverse = arr.reverse();
+
+arrInt = Number(reverse.join(''));
+
+console.log(arrInt);
+
+
+*/
